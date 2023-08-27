cbcd7fb06866995da966651e8faf4c226027cfcc
vivek Latwal <vivek@coadjute.com>
2023-08-27 21:10:51 +0530

    thirf ignore lock

--- database.js
+++ database.js
@@ -2,10 +2,17 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
+// console.log(
+//   process.env.DB_HOST,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   process.env.DB_DATABASE,
+//   "vinay"
+// );
 export const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
